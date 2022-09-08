c20f57aeff540190e0be4b029e88fd957c8e8209
mrplayzorro <102983952+mrplayzorro@users.noreply.github.com>
2022-09-08 15:16:16 +0200

    Create FORKING BORONIDE IS NOW ILLEGAL.lua
    FORKING BORONIDE IS NOW ILLEGAL

---
+++ FORKING BORONIDE IS NOW ILLEGAL.lua
@@ -0,0 +1,8 @@
+local UPS = "boronide"
+local boronide = "boronide"
+local player = game.Player.Localplayer
+if boronide == UPS then
+  print("FORKING BORONIDE IS NOW ILLEGAL")
+  wait(69)
+  player:Kick("FORKING BORONIDE IS NOW ILLEGAL")
+end
